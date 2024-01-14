f2b0ff1e2042832a56c116f777bcec6ed1d98c04
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2024-01-14 23:13:30 +0000

    Have modified the code

---
+++ TestInPython/unittest1.py
@@ -0,0 +1,30 @@
+
+#Here's one of implementatuon of  a unitest test case:
+import unittest
+
+def add(x,y):
+    return x + y
+
+
+#Each test case class contains one or more test methods
+#which are regular Python methods whose names
+#start with the word 'test'.
+
+class Testaddition(unittest.TestCase):
+
+    def test_add_positive_num(self):
+        res = add(2,3)
+
+        self.assertEqual(res, 5)
+
+
+    def test_add_negative_num(self):
+        res = add(-2, -3)
+
+        self.assertEqual(res, -5)
+
+#Call the main method
+if __name__ == '__main__':
+    unittest.main()
+
+
