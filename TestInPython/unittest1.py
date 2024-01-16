90fa6705c5cc3ae2df8791a42998c584e1dff7dd
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2024-01-16 13:19:02 +0000

    Have implemneted new code

--- TestInPython/unittest1.py
+++ TestInPython/unittest1.py
@@ -23,6 +23,19 @@ class TestAddition(unittest.TestCase):
 
         self.assertEqual(res, -5)
 
+
+    #The basic systax of the assert statmeent is as follows: assert expression[, message]
+    #expression is the condition that you want to check.
+    #if it evalutes to False, python raises an AssertionError
+    #exception
+    # def divide(a, b):
+    #     assert b != 0, "Division by zero is not allowed"
+    #     return a / b
+    
+    # result = divide(10,2) #No assertion error
+    # result = divide(10, 0) #Raises AssertionError with the message
+
+
 #Call the main method
 if __name__ == '__main__':
     unittest.main()
