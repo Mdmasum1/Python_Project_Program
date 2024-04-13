1e0002d305674736cdf1a18446e5b485d16d827c
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2024-04-13 18:56:27 +0000

    Have changed the code

--- Fast_API/main.py
+++ Fast_API/main.py
@@ -1,17 +1,19 @@
-
-from fastapi import FastAPI
 from typing import Union
 
-#Create fast api object
+from fastapi import FastAPI
+
 app = FastAPI()
 
-@app.get("/")  # use this decorator to allow this path or route
-def read_root():
-    return {"Hello": "world"} # use dictionary that automatically convert to json
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+    
+
 
