befa1d70a602cf0bf5e180578622060bcdc0ab2c
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2026-01-27 18:48:45 +0000

    have added new file

--- Fast_API/book/books.py
+++ Fast_API/book/books.py
@@ -1,9 +1,11 @@
-#Create my first APi endpoints
-from fastapi import FastAPI
+
+from fastapi import FastAPI 
+
 
 app = FastAPI()  #Create fast api object
 
 
+@app.get("/api-endpoint")
 async def first_api():
     return {"message": "Hello Eric!"}
 
