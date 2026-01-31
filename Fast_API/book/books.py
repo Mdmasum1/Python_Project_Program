e871fd14582964f1dbfb7c3d2b319abe3e109c14
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2026-01-31 22:55:43 +0000

    enhance the endpoint and adding some books to retern the books

--- Fast_API/book/books.py
+++ Fast_API/book/books.py
@@ -4,10 +4,21 @@ from fastapi import FastAPI
 
 app = FastAPI()  #Create fast api object
 
+#create a list of books
+BOOKS = [
 
-@app.get("/api-endpoint")
-async def first_api():
-    return {"message": "Hello Eric!"}
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
 
 
 
