b6640d143c07d05512d61871e28e9f02558d3c38
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2026-02-07 17:32:02 +0000

    have implemented new code

--- Fast_API/book/books.py
+++ Fast_API/book/books.py
@@ -20,6 +20,9 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
+@app.get("/books/{dynamic_param}")
+async def read_all_books(dynamic_param):
+   return {'dynamic_param' : dynamic_param}
 
 
 
