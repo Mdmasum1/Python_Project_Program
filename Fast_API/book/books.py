ed44a8a244ac9f321e706d567ec1c5d1ed94dd0e
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2026-02-26 11:27:21 +0000

    have implemented the delete function

--- Fast_API/book/books.py
+++ Fast_API/book/books.py
@@ -57,6 +57,12 @@ async  def udpate_book(updated_book=Body()):
         
   
 
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
 
 
         
