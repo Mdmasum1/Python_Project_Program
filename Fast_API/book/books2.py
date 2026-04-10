f31ff56bb5452c619bb6ee2ac5e27ea404f043f2
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2026-04-10 03:53:05 +0000

    have changed the code

--- Fast_API/book/books2.py
+++ Fast_API/book/books2.py
@@ -2,7 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+#Here , we are going to create new book object from class Book
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    #Create constructor
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.rating = rating
+
+# Inside BOOK , create some book object and return the BOOK
+
 BOOKS = []
+
+
+
 
 
 #Create the first APi endpoint 
